0b95f7d5fa2897974c25ab9f50fe266cfea61293
Jao1229 <162514087+Jao1229@users.noreply.github.com>
2025-09-30 19:35:59 -0300

    Update horta_dml.sql

--- horta_dml.sql
+++ horta_dml.sql
@@ -11,3 +11,15 @@ values (1, "Sábados pela manhã", 5);
 
 insert into Coordenador (id_usuario, funcao, area_responsavel) 
 values (2, "Gestão", "Área Norte");
+
+
+insert into Visitante (id_usuario, data_visita) 
+values (3, "2025-09-28");
+
+select * from Voluntario; 
+select * from Coordenador; 
+select * from Visitante;
+
+update Voluntario 
+set tempo_colaboracao = tempo_colaboracao + 10 
+where id_usuario = 1;

02bdaa602584efc622d8800bbd5b4203bc36f3f1
Jao1229 <162514087+Jao1229@users.noreply.github.com>
2025-09-30 19:40:00 -0300

    Update horta_dml.sql

--- horta_dml.sql
+++ horta_dml.sql
@@ -1,3 +1,4 @@
+-- Usuario --
 insert into Usuario (nome, email, telefone, tipo) 
 values ("Maria Silva", "maria@gmail.com", "999999999", "coordenador");
 
@@ -6,13 +7,15 @@ values ("João Pedro", "joao@gmail.com", "888888888", "visitante");
 
 select * from Usuario;
 
+--Voluntario--
 insert into Voluntario (id_usuario, disponibilidade, tempo_colaboracao) 
 values (1, "Sábados pela manhã", 5);
 
+-- Coordenador --
 insert into Coordenador (id_usuario, funcao, area_responsavel) 
 values (2, "Gestão", "Área Norte");
 
-
+-- Visitante --
 insert into Visitante (id_usuario, data_visita) 
 values (3, "2025-09-28");
 
@@ -20,10 +23,12 @@ select * from Voluntario;
 select * from Coordenador; 
 select * from Visitante;
 
+-- Voluntario --
 update Voluntario 
 set tempo_colaboracao = tempo_colaboracao + 10 
 where id_usuario = 1;
 
+-- Parcela --
 update Parcela 
 set status = "ativa" 
 where id_parcela = 2;
@@ -31,5 +36,4 @@ where id_parcela = 2;
 select * from Voluntario;
 select * from Parcela;
 
-set tempo_colaboracao = tempo_colaboracao + 10 
-where id_usuario = 1;
+

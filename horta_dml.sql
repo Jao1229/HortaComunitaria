66541f8243bf7457fb80a9dadb7c5ae3e63ea18a
Jao1229 <162514087+Jao1229@users.noreply.github.com>
2025-09-30 19:35:01 -0300

    Update horta_dml.sql

--- horta_dml.sql
+++ horta_dml.sql
@@ -1,1 +1,7 @@
+insert into Usuario (nome, email, telefone, tipo) 
+values ("Maria Silva", "maria@gmail.com", "999999999", "coordenador");
 
+insert into Usuario (nome, email, telefone, tipo) 
+values ("João Pedro", "joao@gmail.com", "888888888", "visitante");
+
+select * from Usuario;
